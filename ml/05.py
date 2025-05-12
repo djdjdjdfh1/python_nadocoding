4b523a9a93cab0d798c7f6fc188722ec1a937a9a
Park Chan <qkrcks8028@gmail.com>
2025-05-12 22:14:07 +0900

    혼동

--- ml/05.py
+++ ml/05.py
@@ -29,3 +29,45 @@ classifier.predict_proba([[4]]) # 합격할 확률 출력
 y_pred = classifier.predict(X_test)
 print(y_pred) # 예측 값
 print(y_test) # 실제 값 (테스트 세트)
+print(X_test) # 공부 시간 (테스트 세트)
+
+classifier.score(X_test, y_test) # 모델 평가
+# 전체 테스트 세트 4개 중에서 분류 예측을 올바로 맞힌 개수 3개 -> 3/4 = 0.75
+
+X_range = np.arange(min(X), max(X), 0.1) # X 의 최소값에서 최대값까지를 0.1 단위로 잘라서 데이터 생성
+print(X_range)
+p = 1 / (1 + np.exp(-(classifier.coef_ * X_range + classifier.intercept_))) # y = mx + b
+print(p)
+print(p.shape)
+print(X_range.shape)
+p = p.reshape(-1) # 1차원 배열 형태로 변경
+print(p.shape)
+plt.scatter(X_train, y_train, color='blue')
+plt.plot(X_range, p, color='green')
+plt.plot(X_range, np.full(len(X_range), 0.5), color='red') # X_range 개수만큼 0.5 로 가득찬 배열 만들기
+plt.title('Probability by hours')
+plt.xlabel('hours')
+plt.ylabel('P')
+# plt.show()
+
+plt.scatter(X_test, y_test, color='blue')
+plt.plot(X_range, p, color='green')
+plt.plot(X_range, np.full(len(X_range), 0.5), color='red') # X_range 개수만큼 0.5 로 가득찬 배열 만들기
+plt.title('Probability by hours (test)')
+plt.xlabel('hours')
+plt.ylabel('P')
+plt.show()
+
+classifier.predict_proba([[4.5]]) # 4.5 시간 공부했을 때 확률 (모델에서는 51% 확률로 합격 예측, 실제로는 불합격)
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+
+# TRUE NEGATIVE (TN)       FALSE POSITIVE (FP)
+# 불합격일거야 (예측)      합격일거야 (예측)
+# 불합격 (실제)             불합격 (실제)
+
+# FALSE NEGATIVE (FN)      TRUE POSITIVE (TP)
+# 불합격일거야 (예측)      합격일거야 (예측)
+# 합격 (실제)               합격 (실제)
